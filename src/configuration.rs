54fcf5c45af55f4bbba22c0d2249563416923bd4
Tim Hütz <tim+github@janke.biz>
2021-05-07 15:46:31 +0200

    :recycle: Move the configuration code in a separate module

---
+++ src/configuration.rs
@@ -0,0 +1,52 @@
+use serde_derive::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+#[derive(Serialize, Deserialize)]
+pub struct ApiSettings {
+    pub key: String,
+    pub token: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct BoardSettings {
+    pub id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TrelloSettings {
+    pub api: ApiSettings,
+    pub board: BoardSettings,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Configuration {
+    pub trello: TrelloSettings,
+}
+
+pub fn load_configuration() -> Configuration {
+    // try to open the configuration of the program...
+    if let Ok(file) = File::open("velocity.toml") {
+        // ... and read the whole content of the file into memory
+        let mut contents = String::new();
+        let mut file_reader = BufReader::new(file);
+        if let Ok(_) = file_reader.read_to_string(&mut contents) {
+            // if the configuration can be deserialized, return the deserialized object
+            if let Ok(config) = toml::from_str::<Configuration>(&contents) {
+                return config;
+            }
+        }
+    }
+
+    // if we reach this step, we could not read any configuration file and return an empty one
+    Configuration {
+        trello: TrelloSettings {
+            api: ApiSettings {
+                key: "".to_string(),
+                token: "".to_string(),
+            },
+            board: BoardSettings { id: "".to_string() },
+        },
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,7 @@
+use crate::configuration::load_configuration;
 use reqwest::{get, Error as ReqwestError};
-use serde_derive::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
 
-#[derive(Serialize, Deserialize)]
-struct ApiSettings {
-    key: String,
-    token: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct BoardSettings {
-    id: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct TrelloSettings {
-    api: ApiSettings,
-    board: BoardSettings,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Configuration {
-    trello: TrelloSettings,
-}
-
-fn load_configuration() -> Configuration {
-    // try to open the configuration of the program...
-    if let Ok(file) = File::open("velocity.toml") {
-        // ... and read the whole content of the file into memory
-        let mut contents = String::new();
-        let mut file_reader = BufReader::new(file);
-        if let Ok(_) = file_reader.read_to_string(&mut contents) {
-            // if the configuration can be deserialized, return the deserialized object
-            if let Ok(config) = toml::from_str::<Configuration>(&contents) {
-                return config;
-            }
-        }
-    }
-
-    // if we reach this step, we could not read any configuration file and return an empty one
-    Configuration {
-        trello: TrelloSettings {
-            api: ApiSettings {
-                key: "".to_string(),
-                token: "".to_string(),
-            },
-            board: BoardSettings { id: "".to_string() },
-        },
-    }
-}
+mod configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), ReqwestError> {
